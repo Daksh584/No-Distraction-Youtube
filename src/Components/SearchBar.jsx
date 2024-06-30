70a941dd75cfddac76072dbf0035a9e403f06a9a
Daksh584 <22je0292@iitism.ac.in>
2024-06-30 21:44:36 +0530

    UI changes

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -14,7 +14,7 @@ const SearchBar = ({ onSearch }) => {
       <form onSubmit={handleSearch} className="w-full max-w-md">
         <div className="flex items-center border-b border-b-2 border-blue-500 py-2">
           <input
-            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none focus:border-blue-500"
             type="text"
             placeholder="Search YouTube"
             value={query}
@@ -23,13 +23,13 @@ const SearchBar = ({ onSearch }) => {
           <select
             value={searchType}
             onChange={(e) => setSearchType(e.target.value)}
-            className="dropdown"
+            className="select select-bordered max-w-xs"
           >
             <option value="video">Video</option>
             <option value="playlist">Playlist</option>
           </select>
           <button
-            className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+            className="btn btn-primary ml-2"
             type="submit"
           >
             Search
