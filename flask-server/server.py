fd27f46804e5128756e195a3b5b0f01e8029ad0d
Daksh584 <22je0292@iitism.ac.in>
2024-07-14 00:05:15 +0530

    added Hindi Support

--- flask-server/server.py
+++ flask-server/server.py
@@ -8,7 +8,7 @@ CORS(app)  # This will enable CORS for all routes
 @app.route("/<video_id>")
 def video(video_id):
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages = ["en" , "hi" ] )
         return jsonify(transcript)
     except Exception as e:
         return jsonify({"error": str(e)}), 400
